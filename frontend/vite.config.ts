a820e64d551bdbbb5db10472979b8ba43a07c5d1
Priscila Nunes <priscilapereiranunes@gmail.com>
2025-11-16 14:55:25 -0300

    permite que a aplicação seja acessada de qualquer dispositivo na rede
    Signed-off-by: Priscila Nunes <priscilapereiranunes@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Permite acesso de qualquer IP na rede
     port: 3001,
     proxy: {
       '/api': {
